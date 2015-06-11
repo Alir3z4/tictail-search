5ae791330d0685d4767ee00dd5cbf0741b115af6
Alireza Savand <alireza.savand@gmail.com>
2015-06-11 14:20:11 +0400

    Add get_cash $ <3

--- server/utils.py
+++ server/utils.py
@@ -1,10 +1,15 @@
 import csv
+import logging
 import os
 from os.path import join
 from os.path import isfile
 from os import listdir
 
+from werkzeug.contrib.cache import SimpleCache
+
 from server import models
+
+cache = SimpleCache()
 
 
 def load_data():
@@ -40,3 +45,49 @@ def load_data():
         setattr(models, model_name, getattr(models, model_name))
 
     setattr(models.ModelObjectManager, 'raw_data', raw_data)
+
+
+def get_cash(key, timeout=60*60*24, new_value=None, default_value=None):
+    """
+    A simple cache utility method to get cached value based on the provided key.
+    If the the returning value is `None` it will be generated and cached.
+    Of course new value should be provided. So basically this method never
+    return `None` value.
+    Internal cache doesn't have this functionality. `cache.get` will
+    return None or a default value but there's no new generated value.
+
+    :param key: Cache key
+    :type key: str
+
+    :param timeout: Cache value timeout
+    :type timeout: int
+
+    :param new_value: New value to set if the stored cache value doesn't exist.
+    This value will be cached and `default_value` will be ignored.
+    :type new_value: object
+
+    :param default_value: Default value if the stored cache value doesn't exist.
+    This value will be returned and `new_value` will be ignored.
+    :type default_value: object
+    """
+    logging.info(u"[get_cash]: Getting cache for: '{0}'".format(key))
+
+    value = cache.get(key)
+    if value is None:
+        logging.info("[get_cash]: Cache is empty")
+
+        if not new_value and not default_value:
+            return None
+
+        if default_value:
+            logging.info("[get_cash]: Returning `default_value`")
+
+            return default_value
+
+        if new_value:
+            logging.info("[get_cash]: Caching new value")
+            cache.set(key, new_value, timeout)
+
+            return new_value
+
+    return value
