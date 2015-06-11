fbd6af74b0d819a7f428e9d3b79190bec86de45c
Alireza Savand <alireza.savand@gmail.com>
2015-06-11 10:07:35 +0400

    Add docstring for load_data

--- server/utils.py
+++ server/utils.py
@@ -9,7 +9,19 @@ from server import models
 
 def load_data():
     """
-    Loading data
+    Beginning of getting our data into memory.
+
+    In the beginning of app_creation in flask, ``load_data`` will be called
+    walk through ``./data/`` directory to get all the data files which are
+    in `CSV` format.
+
+    CSV files then will be loaded into `ModelObjectManager.raw_data`
+    dictionary. The ``key`` for each data set will be set by their
+    corresponding ``server.models.Model`` name.
+
+    All the data are being casted/converted into hash/dictionary data type.
+
+    :rtype: None
     """
     data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
     data_files = [join(data_path, f) for f in listdir(data_path)
@@ -28,5 +40,3 @@ def load_data():
         setattr(models, model_name, getattr(models, model_name))
 
     setattr(models.ModelObjectManager, 'raw_data', raw_data)
-
-
